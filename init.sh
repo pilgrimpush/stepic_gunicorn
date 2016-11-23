80ccf986ff7c37d5704c69d03baa63783a311054
pilgrimpush <ipushkar@yandex.ru>
2016-11-24 00:15:21 +0300

    second commit

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app
