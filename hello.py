af53302598ff6614b1829dab2b1cb5e64069554b
pilgrimpush <ipushkar@yandex.ru>
2016-11-23 19:02:48 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+def wsgi_application(environ, start_response):
+	
+	status = '200 OK'
+	headers = [ ('Content-type', 'text/plain')]
+	qs = environ['QUERY_STRING'].split('&')
+	start_response(status, headers)
+	qs = [item.encode('utf-8')+b"\n" for item in qs]
+	return qs
---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
