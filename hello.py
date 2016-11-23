48b4993dc8895a7aa64e4cbae0940661d272e99f
pilgrimpush <ipushkar@yandex.ru>
2016-11-24 00:29:41 +0300

    third commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def wsgi_application(environ, start_response):
+def application(environ, start_response):
 	
 	status = '200 OK'
 	headers = [ ('Content-type', 'text/plain')]
--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
+sudo /home/box/web/hello.py /usr/local/lib/python2.7/hello.py
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo gunicorn -b 0.0.0.0:8080 hello
